7a28de2e444c2923c2e73b0a7c02340d9004ec33
Matt McGavern <matt@take2.co>
2020-12-16 11:26:50 -0600

    profile model and tests (#8)

---
+++ models/profile.js
@@ -0,0 +1,38 @@
+import { Model } from 'sequelize';
+import { isString, isNumber } from './validators';
+
+export default (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  Profile.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumber,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isString,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Profile',
+    }
+  );
+  return Profile;
+};
