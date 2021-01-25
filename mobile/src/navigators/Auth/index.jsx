96d6211c5a4ce2adeed44751b787a8d663b94f5d
Matt McGavern <matt@take2.co>
2021-01-25 17:19:59 -0600

    forgot password flow (#32)

--- mobile/src/navigators/Auth/index.jsx
+++ mobile/src/navigators/Auth/index.jsx
@@ -6,6 +6,8 @@ import { defaultScreenOptions as screenOptions } from '../../theme';
 import Welcome from '../../screens/Welcome';
 import SignIn from '../../screens/SignIn';
 import Register from '../../screens/Register';
+import ForgotPassword from '../../screens/ForgotPassword';
+import EmailSent from '../../screens/EmailSent';
 
 const Stack = createStackNavigator();
 
@@ -19,6 +21,8 @@ function WelcomeNavigator() {
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <Stack.Screen name="EmailSent" component={EmailSent} />
     </Stack.Navigator>
   );
 }
